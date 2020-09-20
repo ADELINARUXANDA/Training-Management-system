371f099518ce3ef866281ac56df10b5011c7ec31
ADELINARUXANDA <adelinaruxanda22@gmail.com>
2020-09-20 14:25:12 +0300

    no message

--- training-management-system/src/main/java/com/sda/trainingmanagementsystem/Service/CourseServiceImpl.java
+++ training-management-system/src/main/java/com/sda/trainingmanagementsystem/Service/CourseServiceImpl.java
@@ -1,10 +1,18 @@
 package com.sda.trainingmanagementsystem.Service;
 
 import com.sda.trainingmanagementsystem.Dto.CourseDto;
+import com.sda.trainingmanagementsystem.Repository.CourseRepository;
+import com.sda.trainingmanagementsystem.model.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CourseServiceImpl implements CourseService {
+
+    private CourseRepository courseRepository;
+
+
     @Override
     public List<CourseDto> getCourses() {
         return null;
